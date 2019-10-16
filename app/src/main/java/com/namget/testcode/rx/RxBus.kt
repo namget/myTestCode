3f166b12832932850c12b9615e52aa51af0830e1
yeboykr <yeboykr@bodyfriend.co.kr>
2019-10-16 17:40:53 +0900

    1. RxBus Template

--- app/src/main/java/com/namget/testcode/rx/RxBus.kt
+++ app/src/main/java/com/namget/testcode/rx/RxBus.kt
@@ -3,6 +3,24 @@ package com.namget.testcode.rx
 import io.reactivex.Observable
 import io.reactivex.subjects.PublishSubject
 
+
+/**
+    //데이터 전달
+    RxBus.publish(RxEvent.EventAddPerson(etPersonName.text.toString()))
+    finish()
+
+    //disposable add
+    personDisposable = RxBus.listen(RxEvent.EventAddPerson::class.java).subscribe {
+    adapter.addPerson(person = it.personName)
+    }
+
+    //destory에 등록
+    override fun onDestroy() {
+    super.onDestroy()
+    if (!personDisposable.isDisposed) personDisposable.dispose()
+    }
+ */
+
 object RxBus {
     private val publisher = PublishSubject.create<Any>()
 
--- app/src/main/java/com/namget/testcode/rx/RxEvent.kt
+++ app/src/main/java/com/namget/testcode/rx/RxEvent.kt
@@ -7,20 +7,3 @@ class RxEvent {
     data class aaa(val aa: String)
 }
 
-/**
-    //데이터 전달
-    RxBus.publish(RxEvent.EventAddPerson(etPersonName.text.toString()))
-    finish()
-
-    //disposable add
-    personDisposable = RxBus.listen(RxEvent.EventAddPerson::class.java).subscribe {
-        adapter.addPerson(person = it.personName)
-    }
-
-    //destory에 등록
-    override fun onDestroy() {
-    super.onDestroy()
-    if (!personDisposable.isDisposed) personDisposable.dispose()
-    }
-
-*/
